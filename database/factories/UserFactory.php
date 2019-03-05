c101ea517b3b8bec0fc7303f7727561ecf96d0b6
seephp <Smirsky@ukr.net>
2019-03-05 18:23:58 +0200

    firstuniontest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\User;
+use App\Book;
+use App\Author;
+use App\Publisher;
+use App\Author_Book;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 
@@ -22,5 +26,47 @@ $factory->define(User::class, function (Faker $faker) {
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+    ];
+});
+
+$factory->define(App\Book::class, function (Faker $faker) {
+    return [
+        'publisher_id' => function () {
+            return factory(App\Publisher::class)->create()->id;
+        },
+        'title' => $faker->name,
+        'description' => $faker->sentence,
+        'publish_year' => $faker->year,
+        'cover' => $faker->url,
+
+    ];
+});
+
+$factory->define(App\Author::class, function (Faker $faker) {
+    return [
+        'name' => $faker->name,
+    ];
+});
+
+$factory->define(App\Publisher::class, function (Faker $faker) {
+    return [
+        'title' => $faker->company,
+    ];
+});
+
+$factory->define(App\Publisher::class, function (Faker $faker) {
+    return [
+        'title' => $faker->company,
+    ];
+});
+
+$factory->define(App\AuthorBook::class, function (Faker $faker) {
+    return [
+        'author_id' => function () {
+            return factory(App\Author::class)->create()->id;
+        },
+        'book_id' => function () {
+            return factory(App\Book::class)->create()->id;
+        }
     ];
 });
---
+++ database/migrations/2019_03_05_141207_create_author_book_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('author_book', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('author_id');
+            $table->integer('book_id');
+            $table->unique(['author_id', 'book_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author_book');
+    }
+}
